8f37c7dcedd50e0d62d35306375c446dd8d74063
Gaia94 <riccardigaia94@gmail.com>
2017-10-25 15:01:15 +0200

    Change Transfer title

--- payments.js
+++ payments.js
@@ -1,5 +1,5 @@
 /**
- * @api{post} /transfer/ Transfers money from a user wallet to another
+ * @api{post} /transfer/ Payment to a business
  * @apiParam {Number} amount The amount to be transfer in [eurocents](https://en.wiktionary.org/wiki/Eurocent#English)
  * @apiParam {String} email Email of the beneficiary of the transaction
  * @apiParam {String} password Password of the current logged user
@@ -13,7 +13,7 @@
  * @apiUse AppKey
  * @apiUse Token
  * @apiExample {js} Angular Growish API Client:
- * gwApi.request('transfer').save({ amount:5000,email:"foo@bar.com",password:"password",walletId: "53b157b19a6b51c4048b4572" }).then(function success(response) { }, function error(err) { });
+ * gwApi.request('sendMoney').save({ amount:5000,email:"foo@bar.com",password:"password",walletId: "53b157b19a6b51c4048b4572" }).then(function success(response) { }, function error(err) { });
  */
 
 
