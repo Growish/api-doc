5895380498a74d16de4a3054f664e7feba360123
Lorenzo Colombini <lore.colo@gmail.com>
2017-12-12 13:50:50 +0100

    [api-doc] update documentation

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,1 +1,18 @@
-define({  "name": "Growish API",  "version": "1.0.0",  "description": "Growish API documentation",  "header": {    "title": "Start here",    "content": "<p>Under construction!</p>\n"  },  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2017-11-07T13:29:43.865Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
+define({
+  "name": "Growish API",
+  "version": "1.0.0",
+  "description": "Growish API documentation",
+  "header": {
+    "title": "Start here",
+    "content": "<p>Under construction!</p>\n"
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-12-12T12:50:36.729Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
